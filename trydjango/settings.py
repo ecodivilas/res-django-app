5eba613b1e9a5cdd2bbd4de106595069df62c397
ecodivilas <eleazar.odivilas01@gmail.com>
2024-02-21 11:28:21 +0800

    feature: Add dynamic Secret keys

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z9qnv4ompk8_lbby7-k83)4uithosp(fy*5#g&a$y1pf043(_r'
+# SECRET_KEY = 'django-insecure-z9qnv4ompk8_lbby7-k83)4uithosp(fy*5#g&a$y1pf043(_r'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-z9qnv4ompk8_lbby7-k83)4uithosp(fy*5#g&a$y1pf043(_r')
 print('SECRET_KEY', SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
